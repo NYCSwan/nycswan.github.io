d20e66969a8d0b5c41c162c48bc02c19e74c49b0
Megan Swanby <megan.swanby@gmail.com>
2024-03-27 15:41:20 -0400

    update lint, add testing framework

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+import react from "react";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+      },
+    },
+    ignores: ["dist", "node_modules/**", "eslint.config.mjs"],
+    plugins: { react: react },
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    rules: {
+      semi: "error",
+      "prefer-const": "error",
+      "no-undef": "off",
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    files: ["src/__tests__/**"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    ignores: ["dist", "node_modules/**", "eslint.config.mjs"],
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  appType: "spa",
+  root: ".",
+  build: {
+    chunkSizeWarningLimit: 1024,
+    outDir: "../dist",
+  },
+});
