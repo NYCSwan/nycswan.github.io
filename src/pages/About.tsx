9996a83c7094f2f6ed008883e614e45cee7c51fb
Megan Swanby <megan.swanby@gmail.com>
2025-03-18 09:21:42 -0400

    replace text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
     <ContentContainer>
       <PageTitles
         title={"My Bio"}
-        subtitle="Full Stack Developer. Tinkerer. Thinkerer."
+        subtitle=" A Tinkerer. A Thinkerer. A Maker. A Doer. A Creator."
       />
       <ScrollStack>
         <Typography variant="body1">
@@ -23,20 +23,26 @@ export default function About() {
             }}
           >
             {" "}
-            Megan{" "}
+            Megan.{" "}
           </span>
-          a fullstack developer with a unique background in education and the
-          arts. I am passion about creating beautiful, functional, and intuitive
-          user experiences and products.
         </Typography>
+        <Typography>
+          With a unique background in education, technology and the arts, I am
+          passionate about creating beautiful, functional, and intuitive
+          experiences online and in person. A jack of all trades, master of none
+          is better than master of one!
+        </Typography>
+
         <Typography variant="body1">
-          I&apos;ve been a dev for over 6 years because I love finding solutions
-          for complex real life problems using technology. I have experience
-          working on a variety of projects, from web and mobile apps to IoT. I
-          love working with all types of teams. If it exists, I have probably
-          worked with(in) the organizational structure -- startups to agencies
-          and even non-profits! I most recently tried my hand as a startup
-          founder after having a caretaker
+          After more than 6 years as a software engineer, finding solutions for
+          complex real life problems using technology, I took on an additional
+          role, as a caregiver for my dad who had developed memory issues. It
+          was a challenging and rewarding experience. I have experience working
+          on a variety of projects, from web and mobile apps to IoT. I love
+          working with all types of teams. If it exists, I have probably worked
+          with(in) the organizational structure -- startups to agencies and even
+          non-profits! I most recently tried my hand as a startup founder after
+          having a caretaker
         </Typography>
         <Typography variant="body1">
           I&apos;m a startup evangelist, technology nerd, former public school
